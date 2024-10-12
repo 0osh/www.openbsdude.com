8f52877bee2d18ea1ee781dd3af109e55842ec2d
Oshan Tennakoon <ovtennakoon@gmail.com>
2024-10-12 19:26:37 +1100

    Shortened code

--- script.js
+++ script.js
@@ -17,24 +17,21 @@ let puffyEat = setInterval(() => {
 	puffy.style.rotate = absRot - (flip ?  Math.PI : 0) + "rad";
 	puffy.style.scale = (flip ? -1 : 1) + " 1";
 	
-	let moveY = Math.sin(absRot);
-	let moveX = Math.cos(absRot);
-	puffy.style.left = Number(puffy.style.left.slice(0, -2)) - moveX + "px";
-	puffy.style.top = Number(puffy.style.top.slice(0, -2)) - moveY + "px";
+	puffy.style.left = Number(puffy.style.left.slice(0, -2)) - Math.cos(absRot) + "px";
+	puffy.style.top = Number(puffy.style.top.slice(0, -2)) - Math.sin(absRot) + "px";
 }, 5);
 
 puffy.onmouseover = () => {
 	clearInterval(puffyEat);
 	document.body.style.cursor = "none";
 	puffy.style.rotate = "0rad";
+	puffy.style.pointerEvents = "none";
 	document.body.onmousemove = (e) => {
 		let absRot = Math.atan2(-e.movementY, -e.movementX);
 		let flip = absRot > Math.PI / 2 || absRot < Math.PI / -2;
 		puffy.style.rotate = absRot - (flip ?  Math.PI : 0) + "rad";
 		puffy.style.scale = (flip ? -1 : 1) + " 1";
 		
-		let moveY = Math.sin(absRot);
-		let moveX = Math.cos(absRot);
 		puffy.style.top = e.y - (puffyRect.height / 2) + "px";
 		puffy.style.left = e.x - (puffyRect.width / 2) + "px";
 	}
