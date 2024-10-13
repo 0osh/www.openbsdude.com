a7b636817cd7e8eeb120a8271494eb3d7f929391
Oshan Tennakoon <ovtennakoon@gmail.com>
2024-10-13 17:07:27 +1100

    made puffy only chase cursor if clicked

--- script.js
+++ script.js
@@ -6,33 +6,42 @@ let puffy = docID("puffy");
 let mouseX, mouseY;
 let puffyRect = puffy.getBoundingClientRect();
 
-document.body.onmousemove = (e) => {
-	mouseX = Number(puffy.style.left.slice(0, -2)) - e.x + (puffyRect.width / 2);
-	mouseY = Number(puffy.style.top.slice(0, -2)) - e.y + (puffyRect.height / 2);
-}
+puffy.onclick = () => {
+	puffy.style.position = "absolute";
+	console.log(puffyRect.x);
+	puffy.style.top = puffyRect.y + "px";
+	puffy.style.left = puffyRect.x + "px";
 
-let puffyEat = setInterval(() => {
-	let absRot = Math.atan2(mouseY, mouseX);
-	let flip = absRot > Math.PI / 2 || absRot < Math.PI / -2;
-	puffy.style.rotate = absRot - (flip ?  Math.PI : 0) + "rad";
-	puffy.style.scale = (flip ? -1 : 1) + " 1";
-	
-	puffy.style.left = Number(puffy.style.left.slice(0, -2)) - Math.cos(absRot) + "px";
-	puffy.style.top = Number(puffy.style.top.slice(0, -2)) - Math.sin(absRot) + "px";
-}, 5);
-
-puffy.onmouseover = () => {
-	clearInterval(puffyEat);
-	document.body.style.cursor = "none";
-	puffy.style.rotate = "0rad";
-	puffy.style.pointerEvents = "none";
-	document.body.onmousemove = (e) => {
-		let absRot = Math.atan2(-e.movementY, -e.movementX);
+	let puffyEat = setInterval(() => {
+		let absRot = Math.atan2(mouseY, mouseX);
 		let flip = absRot > Math.PI / 2 || absRot < Math.PI / -2;
 		puffy.style.rotate = absRot - (flip ?  Math.PI : 0) + "rad";
 		puffy.style.scale = (flip ? -1 : 1) + " 1";
 		
-		puffy.style.top = e.y - (puffyRect.height / 2) + "px";
-		puffy.style.left = e.x - (puffyRect.width / 2) + "px";
+		puffy.style.left = Number(puffy.style.left.slice(0, -2)) - Math.cos(absRot) + "px";
+		puffy.style.top = Number(puffy.style.top.slice(0, -2)) - Math.sin(absRot) + "px";
+	}, 5);
+	
+	puffy.onmouseenter = () => {
+		clearInterval(puffyEat);
+		document.body.style.cursor = "none";
+		puffy.style.rotate = "0rad";
+		puffy.style.pointerEvents = "none";
+		document.body.onmousemove = (e) => {
+			let absRot = Math.atan2(-e.movementY, -e.movementX);
+			let flip = absRot > Math.PI / 2 || absRot < Math.PI / -2;
+			puffy.style.rotate = absRot - (flip ?  Math.PI : 0) + "rad";
+			puffy.style.scale = (flip ? -1 : 1) + " 1";
+			
+			puffy.style.top = e.y - (puffyRect.height / 2) + "px";
+			puffy.style.left = e.x - (puffyRect.width / 2) + "px";
+		}
 	}
+	
+	puffy.onclick = null;
+}
+
+document.body.onmousemove = (e) => {
+	mouseX = Number(puffy.style.left.slice(0, -2)) - e.x + (puffyRect.width / 2);
+	mouseY = Number(puffy.style.top.slice(0, -2)) - e.y + (puffyRect.height / 2);
 }
